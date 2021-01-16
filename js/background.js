5ccc68ae46a1826997213cbc2ee63c84fd0dc73f
Mathieu Jadin <mathjadin@gmail.com>
2021-01-16 18:02:30 +0100

    Set the note and background default font size to 18

--- js/background.js
+++ js/background.js
@@ -46,6 +46,6 @@ function add_summernote_listeners(base = $(document)) {
 
 $(_ => {
     // Recovers the content inside the textarea and build a WYSWYG GUI
-    $('#background').summernote(summernote_cfg)
+    $('#background').summernote(summernote_cfg).summernote('fontSize', 18)
     add_summernote_listeners()
 })
