659e6c52237d25422f0713e580e564a70deb4238
Mathieu Jadin <mathjadin@gmail.com>
2021-01-28 11:08:14 +0100

    Remove filter on origin level for ki powers
    Indeed the psychic concentration
    depends on meditation talent
    which is a -4 talent (but could be
    elsewhere in other universes)

--- js/talent_med_fantasy.js
+++ js/talent_med_fantasy.js
@@ -4,7 +4,7 @@ default_talents = {
         "Menuiserie/Ébénisterie", "Minéralogie/Joaillerie", "Poisons", "Serrurerie", "Zoologie"],
     "-4": ["Armes normales", "Arts martiaux/Lutte", "Camouflage", "Commerce/Marchandage", "Coutumes étrangères",
         "Équitation", "Géographie", "Langue étrangère proche", "Jonglage/Acrobaties", "Maçonnerie",
-        "Médecine", "Musique", "Natation", "Piégeage", "Vol à la tire"],
+        "Médecine", "Méditation", "Musique", "Natation", "Piégeage", "Vol à la tire"],
     "-2": ["Armes légères", "Bouclier", "Bricolage", "Cartographie", "Comédie", "Danse", "Déguisement", "Dessin",
         "Dressage", "Escalade", "Orientation", "Poésie/Contes & Légendes", "Pistage/Chasse/Pêche", "Premiers soins",
         "Recherches en bibliothèque", "Religion", "Sculpture"],
