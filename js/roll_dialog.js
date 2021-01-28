c4c2a4a81296bf4509176f145e7fd83259d409e9
Mathieu Jadin <mathjadin@gmail.com>
2021-01-28 10:53:38 +0100

    Allow "-[B+2]PV" expressions
    Without that, players needed to add a whitespace
    before and after [B+2] to make replacement work.

--- js/roll_dialog.js
+++ js/roll_dialog.js
@@ -52,7 +52,7 @@ function has_any_base_energy() {
     }).length > 0
 }
 
-const effect_column_regex = /(^|\W)\[([ABCDEFGHIJK])([+-]\d+)?](\W|$)/gi
+const effect_column_regex = /(^|\W?)\[([ABCDEFGHIJK])([+-]\d+)?](\W?|$)/gi
 const effect_margin_regex = /(^|\W)(MR)(\W|$)/gi
 const effect_dss_regex = /(^|\W)(DSS)(\W|$)/gi
 const effect_des_regex = /(^|\W)(DES)(\W|$)/gi
