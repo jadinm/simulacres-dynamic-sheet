jadinm/simulacres-dynamic-sheet

commit 5519cb8a70097ff49c20aea5aaacd25d05ccfae2

Fix replacing of MR that generates new lines
We remove some useless span elements to do so

js/roll_dialog.js
@@ -535,8 +535,8 @@ class TalentRoll extends Roll {
                     + this.dice_buttons("effect_dices", this.effect_dices) + "</div>"
             } else if (!discovery) {
                 // DSS = MR // 3 and DES = 0 or (1 if 4 <= ME <= 6) or (2 if ME >= 7)
-                effect_text += "<div class='row mx-1 align-middle'>DSS =&nbsp;<span class='roll-dialog-dss'>" + this.dss()
-                    + "</span></div><div class='row mx-1 align-middle'>DES =&nbsp;<span class='roll-dialog-des'>" + this.des() + "</span></div>"
+                effect_text += "<div class='row mx-1 align-middle'>DSS =&nbsp;" + this.dss()
+                    + "</div><div class='row mx-1 align-middle'>DES =&nbsp;" + this.des() + "</div>"
             }
         }
 
@@ -701,27 +701,21 @@ class TalentRoll extends Roll {
             if (is_v7) {
                 // Show the actual effect instead of [A] or [B+2]
                 effect = effect.replaceAll(effect_column_regex, (match, prefix, column, modifier, suffix) => {
-                    prefix = prefix.replace(" ", "&nbsp;")
                     modifier = typeof modifier === "undefined" ? 0 : parseInt(modifier.replaceAll(" ", ""))
                     const effect_value = this.is_success() ? this.column_effect(column, modifier) : 0
-                    suffix = suffix.replace(" ", "&nbsp;")
-                    return prefix + "<span class='roll-dialog-effect' column='" + column + "' " + "modifier='" + modifier + "'>" + effect_value + "</span>" + suffix
+                    return prefix.replace(" ", "&nbsp;") + effect_value + suffix.replace(" ", "&nbsp;")
                 })
             } else {
                 // Update with the DSS if "DSS" is in the text
                 effect = effect.replaceAll(effect_dss_regex, (match, prefix, _, suffix) => {
-                    return prefix.replace(" ", "&nbsp;") + "<span class='roll-dialog-dss'>" + this.dss() + "</span>"
-                        + suffix.replace(" ", "&nbsp;")
+                    return prefix.replace(" ", "&nbsp;") + this.dss() + suffix.replace(" ", "&nbsp;")
                 })
                 effect = effect.replaceAll(effect_des_regex, (match, prefix, _, suffix) => {
-                    return prefix.replace(" ", "&nbsp;") + "<span class='roll-dialog-des'>" + this.des() + "</span>"
-                        + suffix.replace(" ", "&nbsp;")
+                    return prefix.replace(" ", "&nbsp;") + this.des() + suffix.replace(" ", "&nbsp;")
                 })
             }
             effect = effect.replaceAll(effect_margin_regex, (match, prefix, _, suffix) => {
-                return prefix.replace(" ", "&nbsp;")
-                    + "<span class='roll-dialog-margin'>" + this.post_test_margin() + "</span>"
-                    + suffix.replace(" ", "&nbsp;")
+                return prefix.replace(" ", "&nbsp;") + this.post_test_margin() + suffix.replace(" ", "&nbsp;")
             });
         }
         effect += "</div>"

commit d135ba2acadc624f94f1b1899e75abe04c8aa755

Fix v7 effect modifier
Bug introduced in commit 5519cb8a

js/roll_dialog.js
@@ -1188,17 +1188,9 @@ function slider_value_changed_event(e) {
         timed_update_roll_trigger()
     } else if (input.id === "roll-dialog-effect-modifier") {
         current_roll.effect_modifier = modifier
+        current_roll.show_roll(true)
         timed_update_roll_trigger()
     }
-
-    // Update with scaled effect in the text
-    $(".roll-dialog-effect").each((i, elem) => {
-        const column = elem.getAttribute("column")
-        let column_modifier = elem.getAttribute("modifier")
-        column_modifier = column_modifier.length > 0 ? parseInt(column_modifier) : 0
-        const effect_value = current_roll.column_effect(column, column_modifier)
-        $(elem).html(effect_value)
-    })
 }
 
 $(_ => {