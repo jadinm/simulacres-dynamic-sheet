jadinm/simulacres-dynamic-sheet

commit 82490e7cae2af2cf7da37b60ab4aecffe5555347

Optimize import
Do not trigger a change if the value did not changed on import

js/import_export.js
@@ -244,8 +244,10 @@ function import_data(src_html, dst_html, full_sheet) {
                         update_talent({item: row[0], to: list[0]})
                     } else {
                         // We change the value and trigger the change in case of a listener
-                        new_input.val(old_input.value)
-                        new_input.trigger("change")
+                        if (new_input[0].value !== old_input.value) {
+                            new_input.val(old_input.value)
+                            new_input.trigger("change")
+                        }
                     }
                 }
             } else {
@@ -276,8 +278,10 @@ function import_data(src_html, dst_html, full_sheet) {
                     }
                 } else {
                     // We change the value and trigger the change in case of a listener
-                    new_input.val(old_input.value)
-                    new_input.trigger("change")
+                    if (new_input.length > 0 && new_input[0].value !== old_input.value) {
+                        new_input.val(old_input.value)
+                        new_input.trigger("change")
+                    }
                 }
             }
             if (new_input.length === 0 && !old_input.id.includes("plugin-") && !old_input.id.includes("note-dialog-") && !old_input.id.includes("ColorPicker")) { // The old input is lost

commit ce2d6a6ba961b0ff9f66cde2eb19f60981ef4518

Fix sliders values that are not imported
The fixed bug was introduced in 82490e7c

js/import_export.js
@@ -278,7 +278,8 @@ function import_data(src_html, dst_html, full_sheet) {
                     }
                 } else {
                     // We change the value and trigger the change in case of a listener
-                    if (new_input.length > 0 && new_input[0].value !== old_input.value) {
+                    if (new_input.length > 0 && new_input[0].value !== old_input.value
+                        || new_input.hasClass("input-slider")) {
                         new_input.val(old_input.value)
                         new_input.trigger("change")
                     }