5762bacff72e186e7ba1a3027dfb0fc863df0551
Mathieu Jadin <mathjadin@gmail.com>
2021-04-02 11:13:31 +0200

    Add a few talents

--- js/talent_med_fantasy.js
+++ js/talent_med_fantasy.js
@@ -2,12 +2,13 @@ default_talents = {
     x: ["Agriculture", "Alchimie", "Art de la guerre", "Art magique", "Botanique", "Histoire",
         "Langue étrangère éloignée", "Lire & Écrire", "Lire sur les lèvres", "Mathématiques", "Navigation",
         "Menuiserie/Ébénisterie", "Minéralogie/Joaillerie", "Poisons", "Serrurerie", "Zoologie"],
-    "-4": ["Armes normales", "Arts martiaux/Lutte", "Camouflage", "Commerce/Marchandage", "Coutumes étrangères",
-        "Équitation", "Géographie", "Langue étrangère proche", "Jonglage/Acrobaties", "Maçonnerie",
-        "Médecine", "Méditation", "Musique", "Natation", "Piégeage", "Vol à la tire"],
+    "-4": ["Armes normales", "Arts martiaux/Lutte", "Camouflage", "Connaissance de la rue",
+        "Commerce/Marchandage", "Coutumes étrangères", "Équitation", "Étiquette & Héraldique", "Géographie",
+        "Langue étrangère proche", "Jonglage/Acrobaties", "Maçonnerie", "Médecine", "Méditation", "Musique",
+        "Natation", "Piégeage", "Vol à la tire"],
     "-2": ["Armes légères", "Bouclier", "Bricolage", "Cartographie", "Comédie", "Danse", "Déguisement", "Dessin",
-        "Dressage", "Escalade", "Orientation", "Poésie/Contes & Légendes", "Pistage/Chasse/Pêche", "Premiers soins",
-        "Recherches en bibliothèque", "Religion", "Sculpture"],
+        "Dressage", "Escalade", "Orientation", "Mémoire", "Poésie/Contes & Légendes", "Pistage/Chasse/Pêche",
+        "Premiers soins", "Recherches en bibliothèque", "Religion", "Sculpture"],
     "0": ["Athlétisme", "Bagarre", "Chant", "Cuisine", "Discrétion", "Entregent", "Langue maternelle", "Séduction",
         "Observation"]
 }
